8a5892af1a64e3bf5ac09c67fb16f3e22c2dca04
RithmaRandima <rithma.r.nandasiri.com>
2025-07-07 07:57:08 +0530

    Updated Version with Attachments

--- Index.js
+++ Index.js
@@ -1,6 +1,11 @@
 import "dotenv/config";
 import { log } from "node:console";
 import { createTransport } from "nodemailer";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const transpoter = createTransport({
   service: "gmail",
@@ -16,6 +21,29 @@ transpoter.sendMail(
     to: process.env.TO_EMAIL,
     subject: "Test Email",
     text: "This is my first Node Mail Project with nodemailer",
+    html: `
+    <html>
+      <head></head>
+      <body>
+        <div style="font-family: Arial, sans-serif; padding: 20px; background-color: #f7f7f7; color: #333;">
+          <h2 style="color: #0056b3; margin-bottom: 10px;">Hello from Nodemailer 👋</h2>
+          <p style="font-size: 16px; line-height: 1.5; margin: 0 0 10px;">
+            This is a <strong style="color: #d6336c;">sample HTML email</strong> sent using Nodemailer through Outlook SMTP.
+          </p>
+          <a href="https://example.com" style="display: inline-block; padding: 10px 20px; background-color: #28a745; color: #fff; text-decoration: none; border-radius: 4px;">
+          Visit Our Site
+          </a>
+        </div>
+      </body>
+    </html>
+    
+    `,
+    attachments: [
+      {
+        filename: "mail.txt",
+        path: join(__dirname, "mail.txt"),
+      },
+    ],
   },
   (err, info) => {
     if (err) {
